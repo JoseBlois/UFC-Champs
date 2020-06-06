8506428aec1726a422c8d1e69a70fd3ca0e93b5a
jose blois <joseblois3@gmail.com>
2020-06-06 09:58:06 -0300

    Just added another drag target so the user can have a fighter wish list

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
-var target = document.querySelector('#drag-target');
-// console.log(target)
+var target = document.querySelectorAll('.drag-target');
+console.log(target)
 var cards = document.querySelectorAll('.card');
 // console.log(cards)
 
 
 var styleCheck = function(id){
-    if(id === '185' || id === '145' || id === '265'){
+    if(id === '185' || id === '145' || id === '265' || id === '205'){
         return 'Striker';
     }else{
         return 'Wrestler'
@@ -14,7 +14,7 @@ var styleCheck = function(id){
 
 var dragStart = function(e){
     e.dataTransfer.setData('text/plain', e.target.id)
-    console.log(e.target)
+    // console.log(e.target)
 };
 
 var cancel = function(e) {
@@ -27,15 +27,24 @@ var dropped = function(e){
     var id;
     e.preventDefault();
     id = e.dataTransfer.getData('text/plain');
+    // console.log(this.id);
+    if(this.id == 'drag-target-style'){
+        viewerUpdate(id)
+    }
+}
+var viewerUpdate = function(id){
     var text = styleCheck(id);
-    var elemento = document.getElementById('text')
+    var elemento = document.getElementById('text-style')
     // console.log(elemento)
     elemento.innerHTML=(text);
     // console.log(text);
 }
-target.addEventListener('dragenter', cancel, false);
-target.addEventListener('dragover', cancel, false)
-target.addEventListener('drop',dropped,false);
 for (let i = 0, l = cards.length; i<l ;i ++){
     cards[i].addEventListener('dragstart',dragStart,false);
+}
+for (let v = 0, b = target.length; v<b ;v ++){
+target[v].addEventListener('dragenter', cancel, false);
+target[v].addEventListener('dragover', cancel, false)
+target[v].addEventListener('drop',dropped,false);
+console.log(target[v])
 }
