ab0bca44bdbad150874943f8150555ed84c93770
jose blois <joseblois3@gmail.com>
2020-06-07 07:44:54 -0300

    added functionality to figther wish list

--- js/app.js
+++ js/app.js
@@ -1,8 +1,29 @@
 var target = document.querySelectorAll('.drag-target');
 console.log(target)
 var cards = document.querySelectorAll('.card');
+var fightersList = document.querySelector('#drag-target-wish ul');
+console.log(fightersList);
 // console.log(cards)
 
+//wishlist constructor
+var List = (function(){
+    this.fighters = [];
+});
+//fighter constructor
+var Fighter =( function(name){
+    this.name = name;
+})
+var currentList = null;
+currentList = JSON.parse(localStorage.getItem('list'))
+if( !currentList){
+    // createEmptyList();
+    createEmptyList();
+}
+UpdateListUI()
+currentList.addFighter = function(fighter){
+    currentList.fighters.push(fighter);
+    localStorage.setItem('list', JSON.stringify(currentList));
+}
 
 var styleCheck = function(id){
     if(id === '185' || id === '145' || id === '265' || id === '205'){
@@ -30,6 +51,8 @@ var dropped = function(e){
     // console.log(this.id);
     if(this.id == 'drag-target-style'){
         viewerUpdate(id)
+    }else{
+        addFighter(id);
     }
 }
 var viewerUpdate = function(id){
@@ -39,6 +62,33 @@ var viewerUpdate = function(id){
     elemento.innerHTML=(text);
     // console.log(text);
 }
+
+var addFighter = function (id){
+    var element = document.getElementById(id);
+    var name = element.getAttribute('data-name');
+
+    var fighter = new Fighter(name);
+    currentList.addFighter(fighter);
+
+    UpdateListUI()
+
+}
+function createEmptyList(){
+    localStorage.clear();
+    localStorage.setItem('list', JSON.stringify(new List()));
+    currentList = JSON.parse(localStorage.getItem('list'));
+}
+
+function UpdateListUI(){
+    fightersList.innerHTML='';
+
+    for(let i = 0, l = currentList.fighters.length; i<l; i++){
+        var liElement = document.createElement('li')
+        liElement.innerHTML = currentList.fighters[i].name;
+        fightersList.appendChild(liElement);
+    }
+}
+
 for (let i = 0, l = cards.length; i<l ;i ++){
     cards[i].addEventListener('dragstart',dragStart,false);
 }
